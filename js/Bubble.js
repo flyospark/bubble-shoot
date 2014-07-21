425a5cd6be1139b64725a519073b6d6a3b1047cd
Archil Imnadze <archil@imnadze.ge>
2014-07-21 20:36:36 +0400

    empty relation properties

--- js/Bubble.js
+++ js/Bubble.js
@@ -1,5 +1,11 @@
 function Bubble (canvasWidth, x, y, shape) {
     return {
+        bottomLeft: null,
+        bottomRight: null,
+        left: null,
+        right: null,
+        topLeft: null,
+        topRight: null,
         distanceTo: function (pointX, pointY) {
             return Math.hypot(x - pointX, y - pointY)
         },
