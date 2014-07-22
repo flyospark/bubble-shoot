a72ea04754267a5f445326c55af7fcdaa93affea
Archil Imnadze <archil@imnadze.ge>
2014-07-22 20:48:30 +0400

    precompute values in bubble shapes

--- js/GreenBubbleShape.js
+++ js/GreenBubbleShape.js
@@ -1,6 +1,8 @@
 function GreenBubbleShape (c, radius) {
 
     var halfRadius = radius / 2
+    var circleRadius = radius - 0.5
+    var fullCircle = Math.PI * 2
 
     var gradient = c.createRadialGradient(0, -halfRadius, 0, 0, -halfRadius, radius)
     gradient.addColorStop(0, 'hsl(100, 100%, 40%)')
@@ -12,7 +14,7 @@ function GreenBubbleShape (c, radius) {
             c.translate(x, y)
             c.fillStyle = gradient
             c.beginPath()
-            c.arc(0, 0, radius - 0.5, 0, Math.PI * 2)
+            c.arc(0, 0, circleRadius, 0, fullCircle)
             c.fill()
             c.restore()
         },
--- js/VioletBubbleShape.js
+++ js/VioletBubbleShape.js
@@ -1,6 +1,8 @@
 function VioletBubbleShape (c, radius) {
 
     var halfRadius = radius / 2
+    var circleRadius = radius - 0.5
+    var fullCircle = Math.PI * 2
 
     var gradient = c.createRadialGradient(0, -halfRadius, 0, 0, -halfRadius, radius)
     gradient.addColorStop(0, 'hsl(300, 100%, 60%)')
@@ -12,7 +14,7 @@ function VioletBubbleShape (c, radius) {
             c.translate(x, y)
             c.fillStyle = gradient
             c.beginPath()
-            c.arc(0, 0, radius - 0.5, 0, Math.PI * 2)
+            c.arc(0, 0, circleRadius, 0, fullCircle)
             c.fill()
             c.restore()
         },
