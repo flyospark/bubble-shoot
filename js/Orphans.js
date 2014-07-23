244cc68fb02f6a233ee71f30fc7ef23be02c8f4d
Archil Imnadze <archil@imnadze.ge>
2014-07-24 01:23:25 +0400

    check all directions (unfinished)

--- js/Orphans.js
+++ js/Orphans.js
@@ -1,8 +1,29 @@
 function Orphans (columns) {
 
+    function checkAndPush (colNumber, rowNumber) {
+
+        var columnBubbles = columnsAndRows[colNumber]
+        if (!columnBubbles) return
+
+        var bubble = columnBubbles[rowNumber]
+        if (!bubble || scannedBubbles[bubble.id]) return
+
+        push(bubble)
+
+    }
+
     function get (colNumber, rowNumber) {
         var columnBubbles = columnsAndRows[colNumber]
         if (columnBubbles) return columnBubbles[rowNumber]
+    }
+
+    function push (bubble) {
+        scannedBubbles[bubble.id] = true
+        orphans.push(bubble)
+        var colNumber = bubble.colNumber
+        var rowNumber = bubble.rowNumber
+        checkAndPush(colNumber + 1, rowNumber + 1)
+        checkAndPush(colNumber - 1, rowNumber + 1)
     }
 
     var columnsAndRows = {}
@@ -15,22 +36,28 @@ function Orphans (columns) {
         }
     }
 
+    var scannedBubbles = {}
     var orphans = []
     for (var i = 0; i < columns.length; i++) {
         var columnBubbles = columns[i]
         for (var j = 0; j < columnBubbles.length; j++) {
 
             var bubble = columnBubbles[j]
+            if (scannedBubbles[bubble.id]) continue
+
             var colNumber = bubble.colNumber
             var rowNumber = bubble.rowNumber
 
             if (!rowNumber) continue
 
-            var topLeftBubble = get(colNumber - 1, rowNumber - 1)
-            var topRightBubble = get(colNumber + 1, rowNumber - 1)
-            if (!topLeftBubble && !topRightBubble) {
-                orphans.push(bubble)
-            }
+            if (get(colNumber - 1, rowNumber - 1)) continue
+            if (get(colNumber + 1, rowNumber - 1)) continue
+            if (get(colNumber - 2, rowNumber)) continue
+            if (get(colNumber + 2, rowNumber)) continue
+            if (get(colNumber - 1, rowNumber + 1)) continue
+            if (get(colNumber + 1, rowNumber + 1)) continue
+
+            push(bubble)
 
         }
     }
