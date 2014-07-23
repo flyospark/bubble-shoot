5f946e71c7ea90f5abda4a9cbb6f3640a5de1c7f
Archil Imnadze <archil@imnadze.ge>
2014-07-23 09:52:09 +0000

    wider area on backgorund

--- js/Background.js
+++ js/Background.js
@@ -1,28 +1,27 @@
-function Background (canvasWidth, canvasHeight, bubbleDiameter) {
+function Background (width, canvasHeight, bubbleDiameter) {
 
     var bubbleRadius = bubbleDiameter / 2
 
     var padding = 8
     var lineWidth = 4
-
+    var lineY = 12
+    var paddedRadius = bubbleRadius + padding
+    var angle = Math.asin(lineY / paddedRadius)
+    var cos = Math.sqrt(1 - Math.pow(lineY / paddedRadius, 2)) * paddedRadius
     var height = bubbleDiameter + padding + lineWidth
+    var halfWidth = width / 2
 
     var canvas = document.createElement('canvas')
-    canvas.width = canvasWidth
+    canvas.width = width
     canvas.height = height
-
-    var paddingRadius = bubbleRadius + padding
-
-    var halfWidth = canvasWidth / 2
 
     var c = canvas.getContext('2d')
     c.lineJoin = c.lineCap = 'round'
     c.translate(halfWidth, height - bubbleRadius)
-    c.moveTo(-halfWidth + lineWidth, 0)
-    c.lineTo(-paddingRadius, 0)
-    c.arc(0, 0, paddingRadius, Math.PI, 0)
-    c.lineTo(paddingRadius, 0)
-    c.lineTo(halfWidth - lineWidth, 0)
+    c.moveTo(-halfWidth + lineWidth, -lineY)
+    c.lineTo(-cos, -lineY)
+    c.arc(0, 0, paddedRadius, -Math.PI + angle, -angle)
+    c.lineTo(halfWidth - lineWidth, -lineY)
     c.strokeStyle = '#555'
     c.lineWidth = lineWidth
     c.stroke()
