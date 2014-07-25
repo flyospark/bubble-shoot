ce7975368e44385354f905092b5e9bc4c5c54038
Archil Imnadze <archil@imnadze.ge>
2014-07-25 20:14:00 +0400

    lighter score

--- run/js/Score.js
+++ run/js/Score.js
@@ -1,8 +1,8 @@
 function Score (canvasWidth, canvasHeight, bubbleDiameter, dpp) {
 
     var score = 0
-    var x = canvasWidth - 11 * dpp
-    var y = canvasHeight - bubbleDiameter + 12 * dpp
+    var x = canvasWidth - 10 * dpp
+    var y = canvasHeight - bubbleDiameter + 10 * dpp
     var font = 'bold ' + (26 * dpp) + 'px Arial, sans-serif'
 
     return {
@@ -13,7 +13,7 @@ function Score (canvasWidth, canvasHeight, bubbleDiameter, dpp) {
             c.textAlign = 'right'
             c.textBaseline = 'top'
             c.font = font
-            c.fillStyle = '#555'
+            c.fillStyle = '#777'
             c.fillText(score, x, y)
         },
         reset: function () {
