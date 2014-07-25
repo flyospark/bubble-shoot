d678f3daa974d1bc646e98a385d7815da3f4c495
Archil Imnadze <archil@imnadze.ge>
2014-07-25 11:39:30 +0000

    fixed bubble trimming issue

--- js/BubbleShape/Green.js
+++ js/BubbleShape/Green.js
@@ -1,25 +1,24 @@
 function BubbleShape_Green (radius) {
 
     var color = 'hsl(100, 100%, 40%)'
+    var halfWidth = radius + 2
 
     var canvas = (function () {
 
-        var diameter = radius * 2
-
         var canvas = document.createElement('canvas')
-        canvas.width = canvas.height = diameter
+        canvas.width = canvas.height = halfWidth * 2
 
         var c = canvas.getContext('2d')
 
         var minusHalfRadius = -radius / 2
 
-        var gradient = c.createRadialGradient(0, minusHalfRadius, 0, 0, minusHalfRadius, diameter)
+        var gradient = c.createRadialGradient(0, minusHalfRadius, 0, 0, minusHalfRadius, radius * 2)
         gradient.addColorStop(0, color)
         gradient.addColorStop(0.5, 'hsl(100, 100%, 30%)')
         gradient.addColorStop(1, color)
 
         c.fillStyle = gradient
-        c.translate(radius, radius)
+        c.translate(halfWidth, halfWidth)
         c.arc(0, 0, radius, 0, Math.PI * 2)
         c.fillStyle = gradient
         c.fill()
@@ -31,7 +30,7 @@ function BubbleShape_Green (radius) {
     return {
         color: color,
         paint: function (c, x, y) {
-            c.drawImage(canvas, x - radius, y - radius)
+            c.drawImage(canvas, x - halfWidth, y - halfWidth)
         },
     }
 
