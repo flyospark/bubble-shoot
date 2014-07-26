3e5d5281802e631680ae6e8345c03c06a8d0e2f1
Archil Imnadze <archil@imnadze.ge>
2014-07-26 10:37:07 +0400

    next bubble opacity

--- run/js/NextBubble.js
+++ run/js/NextBubble.js
@@ -1,19 +1,22 @@
 function NextBubble (canvasWidth, canvasHeight, bubbleRadius, shape) {
 
-    var x = canvasWidth / 2
-    var y = canvasHeight + bubbleRadius
-    var stepIndex = 6
-    var stepSize = bubbleRadius * 2 / stepIndex
+    var x = canvasWidth / 2,
+        y = canvasHeight + bubbleRadius,
+        maxSteps = 8,
+        stepIndex = 0,
+        stepSize = bubbleRadius * 2 / maxSteps
 
     var that = {
         ready: false,
         shape: shape,
         paint: function (c) {
+            c.globalAlpha = stepIndex / maxSteps
             shape.paint(c, x, y)
+            c.globalAlpha = 1
         },
         tick: function () {
-            if (stepIndex) {
-                stepIndex--
+            if (stepIndex < maxSteps) {
+                stepIndex++
                 y -= stepSize
             } else {
                 that.ready = true
