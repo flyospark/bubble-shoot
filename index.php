86a751d1922ac33d50df59b99a8b5b959258ccfe
Archil Imnadze <archil@imnadze.ge>
2014-07-29 13:54:02 +0000

    light border on icons

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+include_once 'fns/get_revisions.php';
+$revisions = get_revisions();
+
 header('Content-Type: text/html; charset=UTF-8');
 
 echo '<!DOCTYPE html>'
@@ -8,12 +11,12 @@ echo '<!DOCTYPE html>'
             .'<title>Bubble Shoot</title>'
             .'<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'
             .'<meta name="viewport" content="width=device-width, user-scalable=no" />'
-            .'<link rel="icon" type="image/png" href="images/icons/16.png" />'
-            .'<link rel="icon" type="image/png" href="images/icons/32.png" sizes="32x32" />'
+            .'<link rel="icon" type="image/png" href="images/icons/16.png?'.$revisions['images/icons/16.png'].'" />'
+            .'<link rel="icon" type="image/png" href="images/icons/32.png?'.$revisions['images/icons/32.png'].'" sizes="32x32" />'
             .'<link rel="stylesheet" type="text/css" href="index.css" />'
         .'</head>'
         .'<body>'
-            .'<img src="images/icons/128.png" />'
+            .'<img src="images/icons/128.png?'.$revisions['images/icons/128.png'].'" />'
             .'<h1>Bubble Shoot</h1>'
             .'<div>A bubble shooting game.</div>'
             .'<a class="button" href="run/">Launch</a>'
--- run/fns/echo_html.php
+++ run/fns/echo_html.php
@@ -1,6 +1,10 @@
 <?php
 
 function echo_html ($html, $head, $body) {
+
+    include_once __DIR__.'/../../fns/get_revisions.php';
+    $revisions = get_revisions();
+
     header('Content-Type: text/html; charset=UTF-8');
     echo '<!DOCTYPE html>'
         .$html
@@ -8,10 +12,11 @@ function echo_html ($html, $head, $body) {
                 .'<title>Bubble Shoot</title>'
                 .'<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />'
                 .'<meta name="viewport" content="width=device-width, user-scalable=no" />'
-                .'<link rel="icon" type="image/png" href="../images/icons/16.png" />'
-                .'<link rel="icon" type="image/png" href="../images/icons/32.png" sizes="32x32" />'
+                .'<link rel="icon" type="image/png" href="../images/icons/16.png?'.$revisions['images/icons/16.png'].'" />'
+                .'<link rel="icon" type="image/png" href="../images/icons/32.png?'.$revisions['images/icons/32.png'].'" sizes="32x32" />'
                 .$head
             .'</head>'
             ."<body>$body</body>"
         .'</html>';
+
 }
