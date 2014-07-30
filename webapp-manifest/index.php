3a52c31687a1632cb6811164788f7142aaab9a08
Archil Imnadze <archil@imnadze.ge>
2014-07-30 22:00:15 +0400

    red laser on icons

--- webapp-manifest/index.php
+++ webapp-manifest/index.php
@@ -20,9 +20,9 @@ echo json_encode([
     'icons' => [
         '16' => '/images/icons/16.png?'.$revisions['images/icons/16.png'],
         '32' => '/images/icons/32.png?'.$revisions['images/icons/32.png'],
-        '90' => '/images/icons/90.png?1',
-        '120' => '/images/icons/120.png?1',
+        '90' => '/images/icons/90.png?2',
+        '120' => '/images/icons/120.png?2',
         '128' => '/images/icons/128.png?'.$revisions['images/icons/128.png'],
-        '256' => '/images/icons/256.png?1',
+        '256' => '/images/icons/256.png?2',
     ],
 ]);
