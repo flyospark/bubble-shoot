d3047a98e1fd8ae7e5e9fb5fbca90935b3911580
Archil Imnadze <archil@imnadze.ge>
2014-07-31 11:14:17 +0000

    improved result canvas

--- run/js/ResultCanvas.js
+++ run/js/ResultCanvas.js
@@ -7,21 +7,28 @@ function ResultCanvas (canvasWidth, canvasHeight, dpp) {
             that.hiding = false
         }
         ratio = index / maxIndex
+        ratioSqr = Math.pow(ratio, 1 / 4)
     }
 
     function showTick () {
         if (index < maxIndex) index++
         else that.showing = false
         ratio = index / maxIndex
+        ratioSqr = Math.pow(ratio, 1 / 4)
     }
 
     var index = 0,
         maxIndex = 24,
         ratio = 0,
-        fontHeight = (26 * dpp),
-        font = 'bold ' + fontHeight + 'px Arial, sans-serif'
+        ratioSqr = 0,
+        largeFontHeight = (48 * dpp),
+        largeFont = 'bold ' + largeFontHeight + 'px Arial, sans-serif',
+        middleFontHeight = (36 * dpp),
+        middleFont = 'bold ' + middleFontHeight + 'px Arial, sans-serif',
+        smallFontHeight = (22 * dpp),
+        smallFont = smallFontHeight + 'px Arial, sans-serif'
 
-    var score, highScore
+    var score, highScore, record
 
     var that = {
         tick: showTick,
@@ -35,30 +42,48 @@ function ResultCanvas (canvasWidth, canvasHeight, dpp) {
         },
         paint: function (c) {
 
-            var ratioSqr = Math.pow(ratio, 1 / 4)
-
             c.fillStyle = 'rgba(0, 0, 0, ' + (ratioSqr * 0.7) + ')'
             c.fillRect(0, 0, canvasWidth, canvasHeight)
 
-            var x = canvasWidth / 2
-            var y = canvasHeight / 4 + canvasHeight * ratioSqr / 4 - fontHeight * 1.5
-            c.translate(x, y)
             c.fillStyle = 'rgba(255, 255, 255, ' + ratioSqr + ')'
             c.textAlign = 'center'
             c.textBaseline = 'top'
-            c.font = font
-            c.fillText('YOUR SCORE: ' + score, 0, 0)
-            if (score > highScore) {
-                c.fillText('NEW RECORD!', 0, fontHeight * 2)
+
+            var x = canvasWidth / 2
+            var y = canvasHeight / 4 + canvasHeight * ratioSqr / 4 - largeFontHeight * 1.5
+            c.save()
+            c.translate(x, y)
+
+            c.font = smallFont
+            c.fillText('YOUR SCORE', 0, 0)
+            c.translate(0, smallFontHeight)
+
+            c.font = largeFont
+            c.fillText(score, 0, 0)
+            c.translate(0, largeFontHeight + smallFontHeight)
+
+            if (record) {
+                c.font = middleFont
+                c.fillStyle = 'hsl(60, 90%, 70%)'
+                c.fillText('NEW RECORD!', 0, 0)
             } else {
-                c.fillText('HIGH SCORE: ' + highScore, 0, fontHeight * 2)
+
+                c.font = smallFont
+                c.fillText('HIGH SCORE', 0, 0)
+                c.translate(0, smallFontHeight)
+
+                c.font = largeFont
+                c.fillText(highScore, 0, 0)
+
             }
-            c.translate(-x, -y)
+
+            c.restore()
 
         },
         show: function (_score, _highScore) {
             score = _score
             highScore = _highScore
+            record = score > highScore
             that.visible = true
             that.hiding = false
             that.tick = showTick
