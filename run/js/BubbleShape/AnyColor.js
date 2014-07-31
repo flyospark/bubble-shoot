5230c5af0e196ae50ad11ffa67ad58a55459dc2b
Archil Imnadze <archil@imnadze.ge>
2014-07-31 08:41:15 +0000

    render AnyColor bubble shape

--- run/js/BubbleShape/AnyColor.js
+++ run/js/BubbleShape/AnyColor.js
@@ -8,33 +8,40 @@ function BubbleShape_AnyColor (radius) {
 
     var c = canvas.getContext('2d')
 
-    var p1x = 0
-    var p1y = radius
+    var redX = 0
+    var redY = radius
 
-    var p2x = radius / 2
-    var p2y = -radius / 2
+    var greenX = radius / 2
+    var greenY = -radius / 2
 
-    var p3x = -radius / 2
-    var p3y = -radius / 2
+    var blueX = -radius / 2
+    var blueY = -radius / 2
 
     var imageData = c.createImageData(size, size)
     for (var y = 0; y < size; y++) {
         for (var x = 0; x < size; x++) {
 
-            var cx = x - halfSize,
-                cy = y - halfSize
+            var centerX = x - halfSize,
+                centerY = y - halfSize,
+                distance = Math.sqrt(centerX * centerX + centerY * centerY) / radius
 
-            var h = Math.sqrt(Math.pow(cx - p1x, 2) + Math.pow(cy - p1y, 2))
-            var r = h / radius
-            r = Math.max(0, Math.min(1, 1 - r)) * 255
+            var angle = Math.atan(centerY / centerX)
+            if (centerX < 0) angle += Math.PI
+            angle -= 0.3
 
-            var h = Math.sqrt(Math.pow(cx - p2x, 2) + Math.pow(cy - p2y, 2))
-            var g = h / radius
-            g = Math.max(0, Math.min(1, 1 - g)) * 255
+            var r = 0, g = 0, b = 0
 
-            var h = Math.sqrt(Math.pow(cx - p3x, 2) + Math.pow(cy - p3y, 2))
-            var b = h / radius
-            b = Math.max(0, Math.min(1, 1 - b)) * 255
+            r = Math.abs(angle - Math.PI / 2) - 1
+            g = -Math.abs(angle - Math.PI / 3) + 2
+            b = -Math.abs(angle - Math.PI * 2 / 3) + 2
+
+            r = Math.max(0, Math.min(1, 1 - r * distance)) * 255
+            g = Math.max(0, Math.min(1, 1 - g * distance)) * 255
+            b = Math.max(0, Math.min(1, 1 - b * distance)) * 255
+
+            r = 16 + r * (255 - 32) / 256
+            g = 16 + g * (255 - 32) / 256
+            b = 16 + b * (255 - 32) / 256
 
             var offset = (y * size + x) * 4
             imageData.data[offset] = r
@@ -45,7 +52,14 @@ function BubbleShape_AnyColor (radius) {
         }
     }
 
-    c.putImageData(imageData, 0, 0)
+    var anotherCanvas = document.createElement('canvas')
+    anotherCanvas.width = anotherCanvas.height = size
+    anotherCanvas.getContext('2d').putImageData(imageData, 0, 0)
+    open(anotherCanvas.toDataURL())
+
+    c.arc(halfSize, halfSize, radius, 0, Math.PI * 2)
+    c.clip()
+    c.drawImage(anotherCanvas, 0, 0)
 
     return {
         isAnyColor: true,
