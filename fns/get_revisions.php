0215911ab55d864ba536c8981ac58f85fa02a765
Archil Imnadze <archil@imnadze.ge>
2015-04-08 11:31:05 +0000

    increase revisions of all icons

--- fns/get_revisions.php
+++ fns/get_revisions.php
@@ -2,13 +2,13 @@
 
 function get_revisions () {
     return [
-        'images/icons/16.png' => 2,
-        'images/icons/32.png' => 3,
-        'images/icons/64.png' => 1,
-        'images/icons/90.png' => 2,
-        'images/icons/120.png' => 2,
-        'images/icons/128.png' => 4,
-        'images/icons/256.png' => 2,
-        'images/icons/512.png' => 1,
+        'images/icons/16.png' => 3,
+        'images/icons/32.png' => 4,
+        'images/icons/64.png' => 2,
+        'images/icons/90.png' => 3,
+        'images/icons/120.png' => 3,
+        'images/icons/128.png' => 5,
+        'images/icons/256.png' => 3,
+        'images/icons/512.png' => 2,
     ];
 }
